a092650ee4edc5abba7147cc36b880eff152493e
Surya R <suryarajendran065@gmail.com>
2026-01-13 21:13:42 -0800

    Image can be uploaded and saved locally. No deletion or access yet.

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 
 app = Flask(__name__)
+LOCAL_IMAGE = "local_image"
+app.config["LOCAL_IMAGE"] = "local_image"
+
+os.makedirs(LOCAL_IMAGE, exist_ok=True)
 
 @app.route("/")
 def home_page():
     return render_template("index.html")
+
+@app.route("/upload_image", methods=["GET", "POST"])
+def upload_image():
+    if request.method == "POST":
+        file = request.files["file"]
+        filepath = os.path.join(app.config["LOCAL_IMAGE"], file.filename)
+        file.save(filepath)
+
+    return redirect(url_for("home_page"))
+
+
+@app.route("/retrieve_image")
+def send_image(filename):
+    return send_from_directory(app.config["LOCAL_IMAGE"], filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
