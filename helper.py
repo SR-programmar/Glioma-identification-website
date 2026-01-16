5eea29f4aac123816af8c120bfef8e603997e494
Surya R <suryarajendran065@gmail.com>
2026-01-15 16:14:00 -0800

    Website can now take in image and make prediction, explaining result to user.

--- helper.py
+++ helper.py
@@ -2,44 +2,55 @@ import tensorflow as tf
 from os import listdir, remove
 from os.path import join, isfile
 
+# Local image directory
+local_dir = "local_image"
+
 
 # Returns true if extension is valid
 def valid_extension(filename):
     extensions = {"png", "jpg", "jpeg"}
     return filename[-3:] in extensions
+
+# Very first image of the local_dir
+def first_img():
+   return join(local_dir, listdir(local_dir)[0])
     
 # Removes the first file from local_image directory
 def clear_dir():
-    
-    path = join("local_image", listdir("local_image")[0])
+    if len(listdir(local_dir)) > 0:
+      path = first_img()
 
-    if isfile(path):
+      if isfile(path):
         remove(path)
 
 # Predicts whether an image has Glioma or not
 def predict_image():
 
-  path = join("local_image", listdir("local_image")[0])
+  path = first_img()
 
   model = tf.keras.models.load_model("glioma_vs_normal.keras")
 
   raw_img = tf.io.read_file(path)
-  raw_img = tf.image.decode_jpeg(raw_img, channels=3)
+
+  # Only PNG and JPEG are acceptable
+  if path[-3:] == "png":
+     raw_img = tf.image.decode_png(raw_img, channels=3)
+  else:
+    raw_img = tf.image.decode_jpeg(raw_img, channels=3)
+  
 
   img = tf.image.resize(raw_img, (224, 224))
   img = tf.expand_dims(img, axis=0)
   predictions = model.predict(img)
   probs = tf.nn.sigmoid(predictions)[0]
 
-  chance_normal = round(probs[0].numpy(), 2) * 100
-  chance_tumor = round(probs[1].numpy(), 2) * 100
+  chance_tumor = round(probs[0].numpy(), 2) * 100
+  chance_normal = round(probs[1].numpy(), 2) * 100
 
-  print(f"Normal: {chance_normal}%")
-  print(f"Tumor: {chance_tumor}%")
 
   if chance_normal > chance_tumor:
-    return f"Normal {chance_normal}% Positive"
+    return f"Normal: {chance_normal}% Positive"
   else:
-    return f"Tumor: {chance_tumor}% Positive"
+    return f"Glioma: {chance_tumor}% Positive"
 
 
--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 ### Modules ###
 
 import os
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
-
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, session, flash
+from helper import clear_dir, predict_image
 # App
 app = Flask(__name__)
+app.secret_key = "12345"
 
 ## Image will be saved here temporarily
 LOCAL_IMAGE = "local_image"
 
 # Set the directory to an environment variable
 app.config["LOCAL_IMAGE"] = "local_image"
+# Set image result to None
 
 # Create local directory
 os.makedirs(LOCAL_IMAGE, exist_ok=True)
@@ -18,29 +20,35 @@ os.makedirs(LOCAL_IMAGE, exist_ok=True)
 # Home Page
 @app.route("/")
 def home_page():
-    return render_template("index.html", file=None)
+    session["result"] = None
+
+    return render_template("index.html", file=None, result=session["result"])
 
 # Home Page
 @app.route("/<filename>")
 def home_page_filename(filename):
-    return render_template("index.html", file=filename)
+    return render_template("index.html", file=filename, result=session["result"])
 
 # Get uploaded file and save it to local
 @app.route("/upload_image", methods=["GET", "POST"])
 def upload_image():
     if request.method == "POST":
+        clear_dir()
         file = request.files["file"]
+        
         filepath = os.path.join(app.config["LOCAL_IMAGE"], file.filename)
+
         file.save(filepath)
         
+        prediction = predict_image()
+        session["result"] = prediction
 
-    return redirect(url_for('home_page_filename', filename=file.filename))
+        return redirect(url_for('home_page_filename', filename=file.filename))
 
 
 # Retrieves an image from local directory to be displayed
 @app.route("/retrieve_image/<filename>")
 def retrieve_image(filename):
-    print(f"File name: {filename}")
     return send_from_directory(app.config["LOCAL_IMAGE"], filename)
 
 # Run 
